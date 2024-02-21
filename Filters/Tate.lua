8f0220d2606b6ebf6571d6a91031a3788b8de6cb
ElRojo <6269516+ElRojo@users.noreply.github.com>
2024-02-21 15:57:44 -0700

    chore: new version, readme, some updates

--- Filters/Tate.lua
+++ Filters/Tate.lua
@@ -1,43 +1,26 @@
-local gamesToFilter = {
-    "Bullet Soul",
-    "Infinite Burst",
-    "DaiOuJou",
-    "Daifukkatsu",
-    "SaiDaiOuJou",
-    "DoDonPachi",
-    "Eschatos",
-    "Espgaluda II",
-    "Ketsui",
-    "Mamoru-Kun",
-    "Muchi Muchi Pork!",
-    "Pink Sweets",
-    "Mushihimesama",
-    "Raiden Fighters Aces",
-    "Raiden IV",
-    "Shikigami No Shiro III",
-    "Shooting 200X",
-    "Shooting 10th Anniversary",
-    "Under Defeat HD",
-    "虫姫さま",
-    "虫姫さまふたり"
-}
+local gamesToFilter = {"BulletSoul", "Bullet Soul", "Bullet Soul Infinite Burst", "Dodonpachi: DaiOuJou - Black Label Extra",
+                       "DoDonPachi: Resurrection Ver 1.5", "Dodonpachi: DaiFukkatsu Ver 1.5",
+                       "Dodonpachi DaiFukkatsu Black Label", "Dodonpachi: SaiDaiOuJou", "Eschatos",
+                       "EspGaluda II - Black Label", "Espgaluda II Black Label", "Espgaluda II: Black Label",
+                       "Ketsui: Kizuna Jigoku Tachi Extra", "Mamoru-Kun", "Muchi Muchi Pork! / (Ibara II): Pink Sweets",
+                       "Mushihimesama HD", "Mushihimesama: Futari Version 1.5", "Raiden Fighters Aces", "Raiden IV",
+                       "Shikigami No Shiro III", "Shooting 200X", "Shooting Love, 200X", "Shooting 10th Anniversary",
+                       "Under Defeat HD", "Shooting Love 200X", "DoDonPachi DaiFukkatsu Black Label",
+                       "DoDonPachi: DaiFukkatsu Black Label", "DoDonPachi SaiDaiOuJou",
+                       "Muchi Muchi Pork! & Pink Sweets", "Mushihimesama", "Mushihimesama Futari",
+                       "DoDonPachi: Resurrection", "虫姫さまふたり", "虫姫さま", "Raiden IV (NA)",
+                       "Raiden IV (JAP)", "Dodonpachi: DaiOuJou Black Label Extra",
+                       "Dodonpachi DaiOuJou Black Label Extra", "Mamorukun Curse!", "Under Defeat HD (JP)", "Under Defeat HD: Deluxe Edition"}
 
-local function matchesGame(title)
-    local lowerTitle = string.lower(title)
-    for _, game in ipairs(gamesToFilter) do
-        if string.find(lowerTitle, string.lower(game), 1, true) then
+local function IsTateable(Content, collection)
+    for _, str in ipairs(collection) do
+        if string.find(string.lower(Content.Name), string.lower(str)) then
             return true
         end
     end
     return false
 end
 
-function filterGames(games)
-    local filteredGames = {}
-    for _, games in ipairs(games) do
-        if matchesGame(game.Title) then
-            table.insert(filteredGames, game)
-        end
-    end
-    return filteredGames
+GameListFilterCategories.User["Tate"] = function(Content)
+    return IsTateable(Content, gamesToFilter)
 end
---
+++ README.md
@@ -0,0 +1,63 @@
+## To install skins
+drop `.xzp` file(s) into `Aurora/skins` folder
+
+## To install coverflows
+drop `.cfljson`  file(s) into `Aurora/media/layouts`
+
+## To install filters
+drop `.lua` file(s) into `Aurora/User/Scripts/Content/Filters`
+
+### Note on filters:
+I suck shit at lua and I'm not super familiar with the built-in methods. Right now, it looks for exact matches using the list below. If your game is not named exactly as shown in this list, it will not pop up. I would like to expand this list (including japanese kanji) so if you can help, please submit a pr!
+
+- "Dodonpachi DaiFukkatsu Black Label", 
+- "Dodonpachi: SaiDaiOuJou", 
+- "Eschatos",
+- "Dodonpachi DaiOuJou Black Label Extra", 
+- "Mamorukun Curse!", 
+- "Under Defeat HD (JP)", 
+- "Under Defeat HD: Deluxe Edition"
+- "DoDonPachi: DaiFukkatsu Black Label", 
+- "DoDonPachi SaiDaiOuJou",
+- "DoDonPachi: Resurrection Ver 1.5", 
+- "Dodonpachi: DaiFukkatsu Ver 1.5",
+- "DoDonPachi: Resurrection", 
+- "虫姫さまふたり", 
+- "虫姫さま", 
+- "Raiden IV (NA)",
+- "EspGaluda II - Black Label", 
+- "Espgaluda II Black Label", 
+- "Espgaluda II: Black Label",
+- "Ketsui: Kizuna Jigoku Tachi Extra", 
+- "Mamoru-Kun", 
+- "Muchi Muchi Pork! / (Ibara II): Pink Sweets",
+- "Muchi Muchi Pork! & Pink Sweets", 
+- "Mushihimesama", 
+- "Mushihimesama Futari",
+- "Mushihimesama HD", 
+- "Mushihimesama: Futari Version 1.5", 
+- "Raiden Fighters Aces", 
+- "Raiden IV",
+- "Raiden IV (JAP)", 
+- "Dodonpachi: DaiOuJou Black Label Extra",
+- "Shikigami No Shiro III", 
+- "Shooting 200X", 
+- "Shooting Love, 200X", 
+- "Shooting 10th Anniversary",
+- "Under Defeat HD", 
+- "Shooting Love 200X", 
+- "DoDonPachi DaiFukkatsu Black Label",
+- "BulletSoul", 
+- "Bullet Soul", 
+- "Bullet Soul Infinite Burst", 
+- "Dodonpachi: DaiOuJou - Black Label Extra"
+
+# Changelog
+
+## tate_v0.1.xzp:
+- First public iteration. Main view and loading screens are rotated. Buttons are mostly replaced throughout.
+
+## tate_v0.2.xzp:
+- Rotated the file manager
+- started work on the filters menu (rb or lb at main screen)
+- Game details mostly works~
